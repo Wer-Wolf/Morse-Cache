075d6d91dfd7debc64f5907ea706d3ca0a940fde
Wer-Wolf <robo-armin@gmx.de>
2019-08-21 16:25:10 +0200

    Veringerung des Overheads durch aufspalten in mehrere Funktionen und Macros

--- battery.h
+++ battery.h
@@ -1,11 +1,11 @@
 #define PULLUP_ENABLE_PIN PB4
 #define ADC_INPUT_PIN PB3
 
-#define START_MEASURING 0
-#define GET_RESULT 1
+#define CALIBRATION_NEEDED 0
+#define NO_CALIBRATION 1
+#define BATTERY_BUSY 2
 
-#define SUCCESS 2
-#define ERROR 3
+#define battery_is_busy() (ADCSRA & (1 << ADSC)) //Wahr solange Messung läuft
 
 uint16_t battery_level = 0;
 
@@ -21,23 +21,28 @@ inline void battery_init() {
     ACSR |= (1 << ACD); //Stromsparen
     DIDR0 |= (1 << ADC3D);
     ADMUX |= (1 << REFS0) | (1 << MUX0) | (1 << MUX1); //1,1V, PB3
-    ADCSRA |= (1 << ADCIE) | (1 << ADPS0) | (1 << ADPS1); //150 kHz
+    ADCSRA |= (1 << ADIE) | (1 << ADPS0) | (1 << ADPS1); //150 kHz
     PRR |= (1 << PRADC);
 }
 
-uint8_t battery(uint8_t action) {
-    switch(action) {
-        case START_MEASURING:
-            PRR &= ~(1 << PRADC);
-            PORTB |= (1 << PULLUP_ENABLE_PIN);
-            ADCSRA |= (1 << ADEN) | (1 << ADSC);
-            return SUCCES;
-        break;
-        default: //GET_RESULT
-            if(ADCSRA & (1 << ADSC)) { //Busy
-                return ERROR;
-            } else {
-                return SUCCES; //battery_level kann gelesen werden
-            }
+void battery_start_measuring() {
+    if(!battery_is_busy()) { //Messung wird nicht unnötig neugestarted
+        PRR &= ~(1 << PRADC);
+        PORTB |= (1 << PULLUP_ENABLE_PIN);
+        ADCSRA |= (1 << ADEN) | (1 << ADSC);
+    }
+}
+
+uint8_t check_for_calibration() {
+    if(!battery_is_busy()) { //Messung wird nicht unterbrochen
+        DDRB &= ~(1 << PULLUP_ENABLE_PIN);
+        if(PINB & (1 << PULLUP_ENABLE_PIN)) {
+            return CALIBRATION_NEEDED;
+        } else {
+            return NO_CALIBRATION;
+        }
+        DDRB |= (1 << PULLUP_ENABLE_PIN);
+    } else {
+        return BATTERY_BUSY;
     }
 }
