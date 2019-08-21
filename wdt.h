76a4588436acb938151c960043d5b7c446a8a635
Wer-Wolf <robo-armin@gmx.de>
2019-08-21 16:34:16 +0200

    Automatischer Reset des WDT bei Prescaleränderung

--- wdt.h
+++ wdt.h
@@ -1,17 +1,17 @@
 #define MS16 0
 #define MS32 1
 #define MS64 2
-#define S0_125 3
-#define S0_250 4
-#define S0_5 5
+#define MS125 3
+#define MS250 4
+#define MS500 5
 #define S1 6
 #define S2 7
 #define S4 32
 #define S8 33
 
-void wdt_set(uint8_t time) {
+inline void wdt_set(uint8_t time) {
     wdt_reset(); //Definierter WDT-Stand
-    WDTCR |= (1 << WDCE);
+    WDTCR |= (1 << WDCE); //KEINE Interrupts!
     WDTCR |= time;
 }
 
