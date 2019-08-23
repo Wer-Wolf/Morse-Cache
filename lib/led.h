9c86900e56533952ae55ab2ff7e5036ff3970569
Wer-Wolf <robo-armin@gmx.de>
2019-08-23 22:08:39 +0200

    removed the ";" in the macros

--- lib/led.h
+++ lib/led.h
@@ -5,5 +5,5 @@
 #define GREEN 2
 #define BOTH 3
 
-#define set_led(color) PORTB |= (color << RED_LED_PIN);
-#define clear_led(color) PORTB &= ~(color << RED_LED_PIN);
+#define set_led(color) PORTB |= (color << RED_LED_PIN)
+#define clear_led(color) PORTB &= ~(color << RED_LED_PIN)
