6254aaf8a58e9d7f3c8a16ed6c1e3022a252b327
Wer-Wolf <robo-armin@gmx.de>
2019-08-31 20:45:57 +0200

    Improve flexibility

--- lib/led.h
+++ lib/led.h
@@ -1,5 +1,11 @@
-#define RED_LED_PIN PB0 //OC0A
-#define GREEN_LED_PIN PB1 //OC0B
+#ifndef RED_LED_PIN
+    #warning RED_LED_PIN automatically defined as PB0
+    #define RED_LED_PIN PB0 //OC0A
+#endif
+#ifndef GREEN_LED_PIN
+    #warning GREEN_LED_PIN automatically defined as PB1
+    #define GREEN_LED_PIN PB1 //OC0B
+#endif
 
 #define RED 1
 #define GREEN 2
