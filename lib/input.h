f2c74d128b4bdf86f2a0fed2dcb6e995b73e3416
Wer-Wolf <robo-armin@gmx.de>
2019-09-03 21:03:37 +0200

    fix a bug where another interrupts also causing the programm to continute

--- lib/input.h
+++ lib/input.h
@@ -1,22 +1,32 @@
 #include <avr/interrupt.h>
+#include <util/atomic.h>
 
 #ifndef INPUT_PIN
     #warning INPUT_PIN automatically defined as PB2
     #define INPUT_PIN PB2 //PCINT2
 #endif
 
-EMPTY_INTERRUPT(PCINT0_vect);
+ISR(PCINT0_vect) {
+    PCMSK &= ~(1 << INPUT_PIN);
+    PORTB &= ~(1 << INPUT_PIN);
+}
+
+#define input_pending() (PCMSK & (1 << INPUT_PIN))
 
 #define input_init() GIMSK |= (1 << PCIE)
 
-void wait_for_input() {
+inline void wait_for_input() {
     PORTB |= (1 << INPUT_PIN); //Kein externer Pullup
+    PCMSK |= (1 << INPUT_PIN);
     set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-    sleep_enable();
-    sleep_bod_disable();
-    PCMSK |= (1 << INPUT_PIN); //Keine Race-Condition
-    sleep_cpu();
-    sleep_disable();
-    PCMSK &= ~(1 << INPUT_PIN);
-    PORTB &= ~(1 << INPUT_PIN);
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+        while(input_pending()) {
+            sleep_enable();
+            sleep_bod_disable();
+            NONATOMIC_BLOCK(NONATOMIC_FORCEOFF) {
+                sleep_cpu();
+            }
+            sleep_disable();
+        }
+    }
 }
