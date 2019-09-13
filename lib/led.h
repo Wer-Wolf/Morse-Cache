5ac8f288777a60507ff0f6cce02a4844207f0ea2
Wer-Wolf <robo-armin@gmx.de>
2019-09-13 21:59:55 +0200

    improve error handling

--- lib/led.h
+++ lib/led.h
@@ -1,10 +1,10 @@
 #ifndef RED_LED_PIN
-    #warning RED_LED_PIN automatically defined as PB0
-    #define RED_LED_PIN PB0 //OC0A
+    #error RED_LED_PIN not defined
+    #define RED_LED_PIN PB0 //Übersichtlichere Fehlermeldung
 #endif
 #ifndef GREEN_LED_PIN
-    #warning GREEN_LED_PIN automatically defined as PB1
-    #define GREEN_LED_PIN PB1 //OC0B
+    #error GREEN_LED_PIN not defined
+    #define GREEN_LED_PIN PB1 //Übersichtlichere Fehlermeldung
 #endif
 
 #define RED 1
