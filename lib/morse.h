7174d1c5ab5347077b1c09a44a3ca2f35306e97a
Wer-Wolf <robo-armin@gmx.de>
2019-09-13 21:35:16 +0200

    fixing a bug causing the DATA_END_ADRESS to be ignored sometimes

---
+++ lib/morse.h
@@ -0,0 +1,44 @@
+#define END_OF_DATA 0xFF
+#define ILLEGAL_DATA 0
+#define DATA_MAX 9
+
+#define convert_to_morse(data) (((uint8_t) ((0xFC1F >> data) & 0x1F)) | (1 << 5))
+
+uint8_t eeprom_get_morse_code() {
+    static uint8_t ee_data_adress = DATA_START_ADRESS;
+    uint8_t raw_data;
+    uint8_t morse_data;
+    do {
+        raw_data = eeprom_read(ee_data_adress);
+        if(raw_data == END_OF_DATA || ee_data_adress >= DATA_END_ADRESS) { //Ende der Daten
+            morse_data = END_OF_DATA;
+            ee_data_adress = 0;
+        } else {
+            if(raw_data > DATA_MAX) {
+                morse_data = ILLEGAL_DATA;
+            } else {
+                morse_data = convert_to_morse(raw_data);
+            }
+            ee_data_adress++;
+        }
+    } while(morse_data == ILLEGAL_DATA);
+    return morse_data;
+}
+
+/*
+Format:
+
+0 = dit (.)
+1 = dah (_)
+
+Beispiel:
+0 0 1 1 1 1 1 0 = 9
+    ^
+    Morsecode (. . . _ _ _ _ .)
+         ^
+         Abschluss des Zeichens (gehöhrt nicht mehr zum Morsecode)
+
+- Von rechts lesen, danach um 1 nach rechts schieben
+
+- Ende wenn gleich 1
+*/
