7e5c5f22e8efbe64d0770629519fc0f0603a1fcc
Wer-Wolf <robo-armin@gmx.de>
2020-03-11 22:59:23 +0100

    preventing message from disapering when file size is greater than 62 characters and reworking user interface

--- tool/eeptool.c
+++ tool/eeptool.c
@@ -6,50 +6,70 @@
 #define ARGS_COUNT_READ 3
 #define ARGS_COUNT_WRITE 4
 #define ARGS_COUNT_MIN ARGS_COUNT_READ
-#define FILENAME argv[1]
-#define ACTION argv[2]
+#define ACTION argv[1]
+#define FILENAME argv[2]
 #define DATA argv[3]
 #define MAX_DIGITS 61
 #define MAX_DATA_LENGHT 62
 #define DATA_END 0x00
 
+#define DATA_MIN 0x30 //"0"
+#define DATA_MAX 0x39 //"9"
+
+#define is_illegal_data(data) ((data) > DATA_MAX || (data) < DATA_MIN)
+
 /*... <FILE> <READ/WRITE> <DATA>*/
 
 int main(int argc, char *argv[]) {
-    printf("eeptool version 1.1\n\n");
+    printf("eeptool v1.1\n\n");
     if(argc < ARGS_COUNT_MIN) {
-        fprintf(stderr, "ERROR: Too few arguments\nUsage: ./eeptool <FILE> <READ/WRITE> <DATA>\n");
+        fprintf(stderr, "ERROR: Too few arguments\nUsage: ./eeptool <read/write> <file> <data>\n");
         return 1;
     } else {
-        if(strcmp(ACTION, "READ") == 0) {
+        if(strcmp(ACTION, "read") == 0) {
             if(argc == ARGS_COUNT_READ) { //Argumente korrekt
                 FILE *file;
-                file = fopen(FILENAME, "rb");
+                file = fopen(FILENAME, "r");
                 if(file == NULL) {
                     fprintf(stderr, "ERROR: Could not read %s\n", FILENAME);
                     return 1;
                 } else {
                     printf("%s sucessfully opened\n", FILENAME);
                     printf("Content of %s: ", FILENAME);
-                    int file_byte;
-                    for(int i = 0; i <= MAX_DIGITS; i++) {
-                        //file_byte = fgetc(file);
-                        if((file_byte = fgetc(file)) == EOF) { //Ende der Datei
-                            fclose(file);
-                            printf("\nNumber of digits: %d\nFinished\n", i);
+                    int file_content = 0;
+                    char byte = 0;
+                    int digit_count = 0;
+                    int i = 0;
+                    for(i = 0; i <= MAX_DIGITS; i++) {
+                        if((file_content = fgetc(file)) == EOF) { //Ende der Datei
                             break;
                         } else {
-                            file_byte = file_byte +  '0'; //uint8_t --> ASCII
-                            printf("%c", file_byte);
+                            if(is_illegal_data(file_content)) {
+                                if(file_content == DATA_END) {
+                                    break;
+                                } else {
+                                    byte = '?'; //Unknown character
+                                }
+                            } else {
+                                byte = file_content;
+                                digit_count++;
+                            }
+                            printf("%c", byte);
                         }
                     }
+                    fclose(file);
+                    printf("\nNumber of digits: %d\n", digit_count);
+                    if(i > digit_count) {
+                        printf("\nWARNING: The eeprom image contains %d possibly damaged data entrys,\nplease consider creating a new eeprom image with 'write'\n\n", i - digit_count);
+                    }
+                    printf("Finished\n");
                 }
             } else {
-                fprintf(stderr, "ERROR: Incorrect using of action READ\nUsage: ./eeptool <FILE> READ\n");
+                fprintf(stderr, "ERROR: Incorrect using of action 'read'\nUsage: ./eeptool read <file>\n");
                 return 1;
             }
         } else {
-            if(strcmp(ACTION, "WRITE") == 0) {
+            if(strcmp(ACTION, "write") == 0) {
                 if(argc == ARGS_COUNT_WRITE) { //Argumente korrekt
                     if(strlen(DATA) > MAX_DIGITS) {
                         fprintf(stderr, "ERROR: Too much data, maximum is %d\n", MAX_DIGITS);
@@ -65,7 +85,7 @@ int main(int argc, char *argv[]) {
                         //Daten sind korrekt
                         printf("Data sucessfully loaded: %s\nNumber of digits: %d\n", DATA, length);
                         FILE *file;
-                        file = fopen(FILENAME, "wb");
+                        file = fopen(FILENAME, "w");
                         if(file == NULL) {
                             fprintf(stderr, "ERROR: Could not access %s\n", FILENAME);
                             return 1;
@@ -73,7 +93,7 @@ int main(int argc, char *argv[]) {
                             printf("Writing data to %s\n", FILENAME);
                             char digit;
                             for(int i = 0; i < length; i++) {
-                                digit = *(DATA + i) - '0'; //ASCII --> uint8_t
+                                digit = *(DATA + i); //Get char
                                 fputc(digit, file);
                                 printf(".");
                             }
@@ -87,7 +107,7 @@ int main(int argc, char *argv[]) {
                         }
                     }
                 } else {
-                    fprintf(stderr, "ERROR: Incorrect using of action WRITE\nUsage: ./eeptool <FILE> WRITE <DATA>\n");
+                    fprintf(stderr, "ERROR: Incorrect using of action 'write'\nUsage: ./eeptool write <file> <data>\n");
                     return 1;
                 }
             } else {
