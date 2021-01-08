c2a7a18081ed4c8a0305a9cda63363e910c365a1
Armin Wolf <W_Armin@gmx.de>
2021-01-08 22:50:47 +0100

    Led: Remove unnecessary #ifndefs
    Remove unnecessary #ifndefs, let the compiler handle
    the warnings.

--- lib/led.h
+++ lib/led.h
@@ -1,14 +1,5 @@
 #pragma once
 
-#ifndef RED_LED_PIN
-    #error RED_LED_PIN not defined
-    #define RED_LED_PIN PB0 //Übersichtlichere Fehlermeldung
-#endif
-#ifndef GREEN_LED_PIN
-    #error GREEN_LED_PIN not defined
-    #define GREEN_LED_PIN PB1 //Übersichtlichere Fehlermeldung
-#endif
-
 #define RED 1
 #define GREEN 2
 #define BOTH 3
