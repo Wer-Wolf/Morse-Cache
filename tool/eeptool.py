2c7d8decf3767e816a78e394d34bf12802703594
Wer-Wolf <robo-armin@gmx.de>
2021-01-13 11:50:40 +0100

    eeptool: Avoid printing trace on common error
    Avoid printing trace on common errors for better usability.

--- tool/eeptool.py
+++ tool/eeptool.py
@@ -14,40 +14,42 @@ parser.add_argument("-w", "--write", type=str, help="convert provided number seq
 parser.add_argument("file", type=str, help="file to read to/write from")
 parser.add_argument("-V", "--version", help="display version number", action="version", version=f"%(prog)s {__version__}")
 args = parser.parse_args()
-
-if args.write is None:
-    with open(args.file, "rb") as fd:
-        damaged_entries = 0
-        digits = 0
-        data = fd.read(MAX_DATA_LENGTH)
-        sys.stdout.write(f"Content of {args.file}: ")
-        for byte in data:
-            if byte == 0x00:
-                sys.stdout.write("\n")
-                break
-            char = chr(byte)
-            if char.isnumeric():
-                sys.stdout.write(char)
-                digits += 1
+try:
+    if args.write is None:
+        with open(args.file, "rb") as fd:
+            damaged_entries = 0
+            digits = 0
+            data = fd.read(MAX_DATA_LENGTH)
+            sys.stdout.write(f"Content of {args.file}: ")
+            for byte in data:
+                if byte == 0x00:
+                    sys.stdout.write("\n")
+                    break
+                char = chr(byte)
+                if char.isnumeric():
+                    sys.stdout.write(char)
+                    digits += 1
+                else:
+                    sys.stdout.write("?")
+                    damaged_entries += 1
             else:
-                sys.stdout.write("?")
-                damaged_entries += 1
-        else:
-            sys.stdout.write("\n")
-            sys.stderr.write("WARNING: Delimiter missing\n")
-        print(f"Digits: {digits}")
-        print(f"Damaged entries: {damaged_entries}")
-        if damaged_entries > 0 or b"\x00" not in data:
-            print("\nWARNING: The EEPROM image is damaged, please consider creating a new EEPROM image")
-elif not args.write.isnumeric():
-    raise ValueError("Only numbers (0-9) are supported")
-elif len(args.write) > MAX_DIGITS:
-    raise ValueError(f"Too many numbers (max is {MAX_DIGITS})")
-else:
-    padding = MAX_DATA_LENGTH - len(args.write)
-    with open(args.file, "w") as fd:
-        fd.write(args.write + "\x00" * padding)
-    print("[" + "#" * len(args.write) + "=" * padding + "]")
-    print(f"{len(args.write)} numbers written")
+                sys.stdout.write("\n")
+                sys.stderr.write("WARNING: Delimiter missing\n")
+            print(f"Digits: {digits}")
+            print(f"Damaged entries: {damaged_entries}")
+            if damaged_entries > 0 or b"\x00" not in data:
+                print("\nWARNING: The EEPROM image is damaged, please consider creating a new EEPROM image")
+    elif not args.write.isnumeric():
+        sys.exit("Only numbers (0-9) are supported")
+    elif len(args.write) > MAX_DIGITS:
+        sys.exit(f"Too many numbers (max is {MAX_DIGITS})")
+    else:
+        padding = MAX_DATA_LENGTH - len(args.write)
+        with open(args.file, "w") as fd:
+            fd.write(args.write + "\x00" * padding)
+        print("[" + "#" * len(args.write) + "=" * padding + "]")
+        print(f"{len(args.write)} numbers written")
+except FileNotFoundError as msg:
+    sys.exit(msg)
 
 sys.exit(0)
